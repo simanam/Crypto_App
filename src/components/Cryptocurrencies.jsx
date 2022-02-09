8c347c93e6cdedd05cab8a80b5622f85ef2e6b60
Aman Singh <amansingh@Amans-MacBook-Pro.local>
2022-02-08 17:25:22 -0800

    Adding crypto app with exchnage page not finished

---
+++ src/components/Cryptocurrencies.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Card, Row, Col, Input } from "antd";
+
+import { useGetCryptosQuery } from "../services/cryptoApi";
+
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
+  const { data: cryptosList, isFetching } = useGetCryptosQuery(count);
+  const [cryptos, setCryptos] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const filteredData = cryptosList?.data?.coins.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    setCryptos(filteredData);
+  }, [cryptosList, searchTerm]);
+
+  if (isFetching) return "Loading..";
+  return (
+    <>
+      {!simplified && (
+        <div className="search-crypto">
+          <Input
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+
+      <Row gutter={[32, 32]} className="crypto-card-contianer">
+        {cryptos?.map((coin) => (
+          <Col xs={24} sm={12} lg={6} className="crypto-card" key={coin.uuid}>
+            <Link to={`/crypto/${coin.uuid}`}>
+              <Card
+                title={`${coin.rank}. ${coin.name}`}
+                extra={<img className="crypto-image" src={coin.iconUrl} />}
+                hoverable
+              >
+                <p>Price: {millify(coin.price)}</p>
+                <p>Market Cap: {millify(coin.marketCap)}</p>
+                <p>Daily Change: {millify(coin.change)}</p>
+              </Card>
+            </Link>
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default Cryptocurrencies;
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./navbar";
+export { default as Homepage } from "./HomePage";
+export { default as Cryptocurrencies } from "./Cryptocurrencies";
+export { default as CryptoDetails } from "./CryptoDetails";
+export { default as News } from "./News";
+export { default as Exchanges } from "./Exchanges";
